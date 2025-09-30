c6cf3a538451f3a400843c56f2259f21808aa703
Bolsolula <160658525+DiegoMaradona03@users.noreply.github.com>
2025-09-30 09:59:07 -0300

    Delete testes1 directory

--- testes1/main.js
+++
@@ -1,6 +0,0 @@
-const mobileButton = document.getElementById('mobile-menu-button');
-const mobileMenu = document.getElementById('mobile-menu');
-
-mobileButton.addEventListener('click', () => {
-    mobileMenu.classList.toggle('hidden');
-});
